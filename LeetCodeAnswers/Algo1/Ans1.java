7caf70d9ec752ea5500963cd512c896139e30bd6
Olawalesegun <olawalesegun0@gmail.com>
2023-08-10 23:43:45 +0100

    LeetCodeAnswers

--- LeetCodeAnswers/Algo1/Ans1.java
+++ LeetCodeAnswers/Algo1/Ans1.java
@@ -1,4 +1,17 @@
 package Algo1;
 
 public class Ans1 {
+    public int[] twoSum(int[] nums, int target) {
+        int lengthOfArray = nums.length;
+        int[] collector = new int[2];
+        for(int firstCount=0; firstCount<lengthOfArray; firstCount++){
+            for(int secondCount = firstCount + 1; secondCount<lengthOfArray; secondCount++){
+                if((nums[firstCount] + nums[secondCount]) == target){
+                    collector[0] = firstCount;
+                    collector[1] = secondCount;
+                }
+            }
+        }
+        return collector;
+    }
 }
