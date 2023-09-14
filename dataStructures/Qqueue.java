1d2c66776457ce0ea0e89417e843da12936fa63f
Olawalesegun <olawalesegun0@gmail.com>
2023-09-14 23:46:19 +0100

    queueue ----------

---
+++ dataStructures/Qqueue.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Qqueue {
+
+    public static void main(String[] args) {
+
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.offer("Shola Badmus");
+        queue.offer("Ejiro Linus");
+        queue.offer("Thambi Fred");
+        queue.offer("Bodmas Quant");
+
+        ;
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.size());
+    }
+}
